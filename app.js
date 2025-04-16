afef0114837ba67f14270e788bfd5ab8b800e25d
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-04-16 20:41:14 +0200

    [UPD] set public folder for static asset

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+//Import express
 const express = require('express')
+//define express onject
 const app = express()
+//set port
 const port = 3000
 
+//Public folder for static asset
+app.use(express.static('public'))
+
+//Response for / route
 app.get('/', (req, res) =>{
     res.send('Server del mio blog!')
 })
 
+//Listening message
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`)
 })
